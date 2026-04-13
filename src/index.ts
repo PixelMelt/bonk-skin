56719eea56969c31dd634b4b4b192325c8a9ef19
PixelMelt <44953835+PixelMelt@users.noreply.github.com>
2026-04-12 21:44:59 -0400

    exports issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export { encodeSkin, decodeSkin } from './skin';
-export { SkinRenderer } from './render/index';
-export type { SkinRendererOptions } from './render/index';
 export { renderToBuffer } from './render/offscreen';
 export type { RenderToBufferOptions } from './render/offscreen';
 export { getBlankSkin, getNewLayer, DEFAULT_BASE_COLOR, DEFAULT_COLORS } from './templates';
@@ -8,3 +6,4 @@ export { validateSkin, validateLayers, decodeAndValidate } from './validate';
 export { teamify, hueify, rgbToHsl, hslToRgb, getDeathColors, TEAM_HUES } from './color';
 export type { HSL } from './color';
 export type { SkinData, SkinLayer, ValidationIssue, ValidationResult } from './types/types';
+export type { SkinRendererOptions } from './render/index';
